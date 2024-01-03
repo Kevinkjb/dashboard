34541841a7da8e508778076f269a0cd7cb0316e3
Kevin <tomfge09@yahoo.ca>
2024-01-02 23:31:22 -0700

    Initial files has been updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
 import { useState } from 'react'
 import './index.css';
 import Employee from './components/Employee';
+import AddEmployee from './components/AddEmployee';
 import { v4 as uuidv4 } from 'uuid';
 
+
 const  App = ()  => {
+
+  //IF FALSE THE DATA WONT SHOW UNLESS IT HAS THE ADMIN ACCESS.
+  // THIS IS HARD CODED VALUE WHICH WILL CHANGE LATER
   const showEmployee = true
-  const [role, setRole] = useState('')
+
   const [employees, setEmployees] = useState(
     [
-      {name: "Kevin", role: "Developer", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
-      {name: "Abby", role: "Developer", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
-      {name: "John", role: "Manager", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
+      {id: 1, name: "Kevin", role: "Developer", img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+      {id: 2, name: "Abby", role: "Developer", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+      {id: 3, name: "John", role: "Manager", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+      {id: 4, name: "Kia", role: "Developer", img: "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+      {id: 5, name: "Joy", role: "Developer", img: "https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+      {id: 6, name: "Ray", role: "Manager", img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"}
     ]
   )
+
+  function updateEmployee(id, newName, newRole){
+    console.log('This is updating')
+
+    //MAP THROUGHT EMPLOYEES TO CHECK THE CONDITION
+    const updateEmployee = employees.map(employee => {
+      if(id === employee.id){
+        //CREATES A NEW ARRAY THAT PASS INTO THE EDIT EMPLOYEE AND REPLACE THE NEW VALUE
+        return {...employee, name: newName, role: newRole}
+      }
+      //ELSE WILL JUST RETURN INTO A PREVIOS OR CURRENT VALUE
+      return employee
+    })
+    //SUMBITS INTO THE UPDATED FUNCTION WHICH SENDS INTO THE EDIT EMPLOYEE
+    setEmployees(updateEmployee)
+  }
+
+
+  // ADDING NEW DATA INTO THE EXISTING OBJECT
+  function NewEmployee(name, role, img){
+    const newEmployee = {
+      //GENERATES A RANDOM ID
+      id: uuidv4(),
+      name: name, //GRABS THE NAME PARAMETER THAT COMES FROM THE COMPONENT ADD EMPLOYEE
+      role: role, //GRABS THE ROLE PARAMETER THAT COMES FROM THE COMPONENT ADD EMPLOYEE
+      img: img, //GRABS THE IMG PARAMETER THAT COMES FROM THE COMPONENT ADD EMPLOYEE
+    };
+    setEmployees([...employees, newEmployee]);
+  }
   return (
     <div className="App">
       { showEmployee ? 
         <>
-          <input className='border-solid border-2 border-gray-500' type="text" onChange={(e) => {
-            setRole(e.target.value)
-          }}/>
           <div className='flex flex-wrap justify-center'>
             {
               employees.map((employee) => (
                 <Employee 
-                  key={uuidv4()}
+                  key={employee.id}
+
+                  //SENDS AN ID INTO THE EDIT EMPLOYEE WHICH CHECKS THE CONDITION IF IT MATCHES
+                  id={employee.id}
+                  //OBJECT DESTRUCTURING WHICH SENDS THE DATA INTO THE EMPLOYEE INFO TO RENDER
                   employees={employee}
+                  //THIS GIVES THE MAIN FILE STATE TO PASS IN OR RENDER INTO THE EMPLOYEE TO UPDATE THE EXISTING DATA
+                  updateEmployee={updateEmployee}
                 />
               ))
             }
@@ -36,6 +76,11 @@ const  App = ()  => {
         )
       }
 
+      {/* THIS IS THE COMPONENT WHICH PASS IN A PROPS INTO THE ADD EMPLOYEE COMPONENT */}
+      <AddEmployee 
+        employees={employees}
+        NewEmployee={NewEmployee}
+      />
     </div>
   )
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,11 +1,14 @@
-const Employee = ({employees}) =>{
+import EditEmployee from "./EditEmployee"
+
+
+const Employee = ({employees, setEmployees, updateEmployee, id}) =>{
     return(
         <>
-        <div className="py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 m-5">
+        <div className="min-w-[350px] max-w-[350px] py-8 px-8  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 m-4">
             <img 
                 className="block mx-auto rounded-full sm:mx-0 sm:shrink-0 object-cover h-[100px] w-[100px]" 
                 src={employees.img}
-                alt="Woman's Face" 
+                alt={employees.name}
             />
             <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
@@ -18,7 +21,16 @@ const Employee = ({employees}) =>{
                     }
                 </div>
                 </div>
-                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+                    <EditEmployee
+                    // THIS ID WAS GRAB MAIN FILE APP TO BE ABLE TO RENDER IT INTO EDIT COMPONENTS
+                    id={id}
+                    // THIS EMPLOYEE GRABS THE MAIN FILE APP TO BE ABLE TO RENDER AND MAP IT INTO EDIT COMPONENTS
+                    employees={employees}
+                    //THIS GRABS THE FUNCTION FROM APP JS TO GIVES AN ACCESS TO EDIT EMPLOYEE COMPONENTS TO RENDER AND ACCESS THE FUNCTION
+                    updateEmployee={updateEmployee}
+                    />
+
+               
             </div>
         </div>
         </>
